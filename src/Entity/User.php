e69c5f0996d0b383253bee5a6c21bc0ca473503a
Pomidorchics <122442733+Pomidorchics@users.noreply.github.com>
2025-11-27 22:14:04 +1000

    Revert "Implemented user authentication and authorization"

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,15 +9,11 @@ use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Override;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
-use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'users')]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -27,29 +23,19 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255)]
     #[Groups(['user:read', 'user:write', 'booking:read'])]
-    #[Assert\NotBlank(message: 'Имя обязательно для заполнения')]
     private ?string $name = null;
 
     #[ORM\Column(length: 255, unique: true)]
     #[Groups(['user:read', 'user:write'])]
-    #[Assert\NotBlank(message: 'Email обязателен для заполнения')]
-    #[Assert\Email(message: 'Некорректный формат email')]
     private ?string $email = null;
 
     #[ORM\Column(length: 20)]
     #[Groups(['user:read', 'user:write', 'booking:read'])]
-    #[Assert\NotBlank(message: 'Телефон обязателен для заполнения')]
     private ?string $phone = null;
 
     #[ORM\Column]
     #[Groups(['user:read'])]
     private ?DateTimeImmutable $createdAt = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
-
-    #[ORM\Column(type: 'json')]
-    private array $roles = [];
 
     /**
      * @var Collection<int, Booking>
@@ -61,7 +47,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $this->bookings = new ArrayCollection();
         $this->createdAt = new DateTimeImmutable();
-        $this->roles = ['ROLE_USER'];
     }
 
     public function getId(): ?int
@@ -115,44 +100,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->createdAt = $createdAt;
 
         return $this;
-    }
-
-    #[Override]
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-        return $this;
-    }
-
-    #[Override]
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
-    }
-
-    public function setRoles(array $roles): static
-    {
-        $this->roles = $roles;
-        return $this;
-    }
-
-    #[Override]
-    public function getUserIdentifier(): string
-    {
-        return (string) $this->phone;
-    }
-
-    #[Override]
-    public function eraseCredentials(): void
-    {
     }
 
     /**
