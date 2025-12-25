b9cf49ce542979f400422303e7fb907d0c7212fc
Pomidorchics <aleksandra180504@gmail.com>
2025-12-25 20:04:51 +1000

    hw6 fixed

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -5,13 +5,54 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Controller\BookingController;
 use App\Repository\BookingRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BookingRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(
+            name: 'get_booking',
+            uriTemplate: '/api/bookings/{id}',
+            controller: BookingController::class . '::getBooking'
+        ),
+        new GetCollection(
+            name: 'get_bookings',
+            uriTemplate: '/api/bookings',
+            controller: BookingController::class . '::getBookings'
+        ),
+        new Post(
+            name: 'create_booking',
+            uriTemplate: '/api/bookings',
+            controller: BookingController::class . '::createBooking'
+        ),
+        new Put(
+            name: 'update_booking',
+            uriTemplate: '/api/bookings/{id}',
+            controller: BookingController::class . '::updateBooking'
+        ),
+        new Patch(
+            name: 'partial_update_booking',
+            uriTemplate: '/api/bookings/{id}',
+            controller: BookingController::class . '::partialUpdateBooking'
+        ),
+        new Delete(
+            name: 'delete_booking',
+            uriTemplate: '/api/bookings/{id}',
+            controller: BookingController::class . '::deleteBooking'
+        )
+    ],
+)]
+
 class Booking
 {
     #[ORM\Id]
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,13 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Controller\UserController;
 use App\Repository\UserRepository;
 use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -18,7 +25,41 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'users')]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(
+            name: 'get_user',
+            uriTemplate: '/api/users/{id}',
+            controller: UserController::class . '::getUser'
+        ),
+        new GetCollection(
+            name: 'get_users',
+            uriTemplate: '/api/users',
+            controller: UserController::class . '::getUsers'
+        ),
+        new Post(
+            name: 'create_user',
+            uriTemplate: '/api/users',
+            controller: UserController::class . '::createUser'
+        ),
+        new Put(
+            name: 'update_user',
+            uriTemplate: '/api/users/{id}',
+            controller: UserController::class . '::updateUser'
+        ),
+        new Patch(
+            name: 'partial_update_user',
+            uriTemplate: '/api/users/{id}',
+            controller: UserController::class . '::partialUpdateUser'
+        ),
+        new Delete(
+            name: 'delete_user',
+            uriTemplate: '/api/users/{id}',
+            controller: UserController::class . '::deleteUser'
+        )
+    ],
+)]
+
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
